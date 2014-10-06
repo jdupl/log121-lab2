766dea1815232fb009a254b52639d4ffa0e814f3
Justin Duplessis <drfoliberg@gmail.com>
2014-10-06 12:27:39 -0400

    Déplacer le comparateur par aire

--- src/main/log121/lab2/formes/comparateurs/ComparateurFormeParAire.java
+++ src/main/log121/lab2/formes/comparateurs/ComparateurFormeParAire.java
@@ -1,6 +1,8 @@
-package main.log121.lab2.formes;
+package main.log121.lab2.formes.comparateurs;
 
 import java.util.Comparator;
+
+import main.log121.lab2.formes.Forme;
 
 public class ComparateurFormeParAire implements Comparator<Forme> {
 
