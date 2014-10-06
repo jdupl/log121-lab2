30db69cfd50a61ebed685dcb6c7dcbd80e58ce56
aviau <alexandre.viau@savoirfairelinux.com>
2014-10-06 10:34:49 -0400

    added bulk methods

--- src/main/log121/lab2/ThreadComm.java
+++ src/main/log121/lab2/ThreadComm.java
@@ -28,7 +28,7 @@ import main.log121.lab2.formes.Forme;
 
 /**
  * SwingWorker qui demande au serveur de formes de nouvelles formes.
- * 
+ *
  * @author Justin Duplessis
  *
  */
@@ -37,18 +37,30 @@ public class ThreadComm extends SwingWorker<Forme, Object> {
 	/**
 	 * Délai entre les demandes en millisecondes
 	 */
-	private static final int DELAI_MSEC = 1000;
+	private int DELAI_MSEC = 1000;
 
 	private InetSocketAddress adresse;
+	private int nbFormes;
 
 	/**
-	 * Constructeur
-	 * 
+	 * ConstructeurdemarrerComm
+	 *
 	 * @param adresse
 	 *            l'adresse du serveur de formes
+	 * @param nbFormes
+	 *            Le nombre de formes à obtenir. -1 pour aucune limite.
 	 */
-	public ThreadComm(InetSocketAddress adresse) {
+	public ThreadComm(InetSocketAddress adresse, int nbFormes, boolean bulk) {
 		this.adresse = adresse;
+		this.nbFormes = nbFormes;
+
+		if (bulk){
+			DELAI_MSEC = 20;
+		}
+		else{
+			DELAI_MSEC = 1000;
+		}
+
 	}
 
 	/**
@@ -62,7 +74,9 @@ public class ThreadComm extends SwingWorker<Forme, Object> {
 			s.connect(adresse);
 			BufferedWriter out = new BufferedWriter(new OutputStreamWriter(s.getOutputStream()));
 			BufferedReader in = new BufferedReader(new InputStreamReader(s.getInputStream()));
-			while (!this.isCancelled()) {
+
+			int compte = 0;
+			while (!this.isCancelled() && compte != this.nbFormes) {
 				try {
 					// Lire prompt ( 'commande>' )
 					in.readLine();
@@ -72,6 +86,7 @@ public class ThreadComm extends SwingWorker<Forme, Object> {
 					// Lire la forme en tant que String
 					String strForme = in.readLine();
 					Forme forme = FormeFactory.lireString(strForme);
+					compte++;
 					// Alerter l'autre classe d'une nouvelle forme
 					firePropertyChange("FORME", null, forme);
 					// Attente avant la prochaine requête au serveur
@@ -98,6 +113,7 @@ public class ThreadComm extends SwingWorker<Forme, Object> {
 			firePropertyChange("ERREUR", null,
 					String.format("Le serveur '%s' a fermé la connexion.", adresse.getHostName()));
 		}
+		this.cancel(false);
 		return null;
 	}
 
